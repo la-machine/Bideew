041f997d8196cdb7d7b8e0e3e10e91071224e123
la-machine <frankpythagore@gmail.com>
2024-04-17 14:24:51 +0100

    home page update

--- src/app/components/main/ze-a1/ze-a1.component.ts
+++ src/app/components/main/ze-a1/ze-a1.component.ts
@@ -1,10 +1,50 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { PodcastService } from 'src/app/service/podcast.service';
+
+const fileUrl = 'https://bideew-2e4c9e77133a.herokuapp.com/api/files'
 
 @Component({
   selector: 'app-ze-a1',
   templateUrl: './ze-a1.component.html',
   styleUrls: ['./ze-a1.component.scss']
 })
-export class ZeA1Component {
+export class ZeA1Component implements OnInit{
+  podcastResponses: any[] = [];
+  latestPodcast:any;
+
+  constructor(private podcastService: PodcastService) { }
+
+  ngOnInit(){
+    this.getPodcast();
+    console.log("displaying the last podcast " + this.latestPodcast)
+  }
+
+  getPodcast() {
+
+    this.podcastService.getAllPodcasts().subscribe(
+      (response) => {
+        this.podcastResponses = response.map(podcast => ({
+          ...podcast,
+          img: `${fileUrl}/${podcast.img}`,
+          audio: `${fileUrl}/${podcast.audio}`,
+
+        }))
+        let latestTicketId = 0;
+        this.podcastResponses.forEach(podcast => {
+          if (podcast.evenType==="Podcast" && podcast.id>latestTicketId) {
+            this.latestPodcast=podcast;
+            latestTicketId = podcast.id;
+          }
+          console.log(podcast.audio);
+          console.log("image ", podcast.img);
+        });
+      },
+      (error) => {
+        console.error('Error fetching podcast:', error);
+        // Handle error, e.g., show an error message or log
+      }
+    );
+  }
 
 }
+

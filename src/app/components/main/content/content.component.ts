7617aca6d8ccdb27ea143c62323da74278c5bc44
la-machine <frankpythagore@gmail.com>
2024-04-24 13:05:58 +0100

    content testing values

--- src/app/components/main/content/content.component.ts
+++ src/app/components/main/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PodcastService } from 'src/app/service/podcast.service';
 
 @Component({
@@ -6,12 +6,16 @@ import { PodcastService } from 'src/app/service/podcast.service';
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit{
 
   podcastResponse: any;
   showFullContent: boolean = false;
 
   constructor(private podcastService: PodcastService){}
+  ngOnInit(): void {
+    this.getDetails();
+    console.log("Showing Podcast description " + this.podcastResponse.desc)
+  }
 
   getDetails(){
     this.podcastResponse = this.podcastService.loadPodcast();
